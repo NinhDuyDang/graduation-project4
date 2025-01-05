f49041fd74ebb67c1797210a9e9c8b6a20ef089c
Ninh Dang <Ninhduy2000@gmail.com>
2025-01-05 17:15:07 +0700

    update

--- src/main/java/com/book_store/service/OrderPDFService.java
+++ src/main/java/com/book_store/service/OrderPDFService.java
@@ -1,9 +1,11 @@
+
 package com.book_store.service;
 import com.book_store.entity.Order;
 import com.book_store.entity.OrderDetail;
 import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
@@ -11,11 +13,11 @@ import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.UnitValue;
 import org.springframework.stereotype.Service;
-
 import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Random;
 
 @Service
 public class OrderPDFService {
@@ -27,75 +29,95 @@ public class OrderPDFService {
             // Tạo PdfWriter và PdfDocument
             PdfWriter writer = new PdfWriter(out);
             com.itextpdf.kernel.pdf.PdfDocument pdfDoc = new com.itextpdf.kernel.pdf.PdfDocument(writer);
+            pdfDoc.setDefaultPageSize(PageSize.A5); // Đặt khổ giấy A5
 
             // Tạo Document
             Document document = new Document(pdfDoc);
-            // Nạp font hỗ trợ Unicode (Thay đổi đường dẫn font theo hệ thống của bạn)
+            document.setMargins(15, 15, 15, 15);
+
+            // Nạp font hỗ trợ Unicode
             String fontPath = "C:/Windows/Fonts/arial.ttf"; // Đường dẫn tới font Arial
             PdfFont font = PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H, true);
-
-            // Áp dụng font vào document
             document.setFont(font);
-            // Thêm tiêu đề
-            document.add(new Paragraph("Hóa đơn ")
-                    .setBold()
-                    .setFontSize(18)
-                    .setTextAlignment(TextAlignment.CENTER)
-                    .setMarginBottom(20));
 
-            // Thêm thông tin đơn hàng
-            document.add(new Paragraph("Mã  đơn hàng: " + order.getId()));
-            document.add(new Paragraph("Ngày tạo: " + order.getCreatedAt().toString()));
-            document.add(new Paragraph("Tên khách hàng: " + order.getCustomer().getName()));
-            document.add(new Paragraph("Phương thức thanh toán: " + (order.getPaymentMethod() == 0 ? "Ship cod" : "Banking")));
+            // Tiêu đề công ty
+            document.add(new Paragraph("Công ty cổ phần sách BookLibrary Hà Nội")
+                    .setBold()
+                    .setFontSize(10)
+                    .setTextAlignment(TextAlignment.CENTER));
+            document.add(new Paragraph("Số 1 Đại Cồ Việt, Hai Bà Trưng, Hà Nội, Việt Nam")
+                    .setFontSize(9)
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setMarginBottom(5));
+
+            // Tiêu đề hóa đơn
+            document.add(new Paragraph("Hóa đơn bán hàng")
+                    .setBold()
+                    .setFontSize(12)
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setMarginBottom(5));
+
+            // Số hóa đơn
+            String invoiceNumber = generateRandomInvoiceNumber(10);
+            document.add(new Paragraph("Số hóa đơn: " + invoiceNumber)
+                    .setFontSize(9)
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setMarginBottom(10));
+
+
+            // Thông tin đơn hàng
             NumberFormat formatter = NumberFormat.getInstance(new Locale("vi", "VN"));
-            document.add(new Paragraph("Tên người nhận: " + order.getReceiverName()));
-            document.add(new Paragraph("Số điện thoại: " + order.getReceiverPhone()));
-            document.add(new Paragraph("Địa chỉ: " + order.getReceiverAddress()));
-            document.add(new Paragraph("Mã giảm giá: " + (order.getPromotion() == null ? "Không có mã giảm giá" : order.getPromotion().getName())));
-            document.add(new Paragraph("Đơn vị vận chuyển: " + order.getShippingUnit().getName()));
-            document.add(new Paragraph("Trạng thái thanh toán: " + (order.getPaymentStatus() == 0 ? "Chưa thanh toán" : "Đã thanh toán")));
+            document.add(new Paragraph("Mã đơn hàng: " + order.getId()).setFontSize(9));
+            document.add(new Paragraph("Ngày tạo: " + order.getCreatedAt().toString()).setFontSize(9));
+            document.add(new Paragraph("Tên khách hàng: " + order.getCustomer().getName()).setFontSize(9));
+            document.add(new Paragraph("Phương thức thanh toán: " + (order.getPaymentMethod() == 0 ? "Ship cod" : "Banking")).setFontSize(9));
+            document.add(new Paragraph("Tên người nhận: " + order.getReceiverName()).setFontSize(9));
+            document.add(new Paragraph("Số điện thoại: " + order.getReceiverPhone()).setFontSize(9));
+            document.add(new Paragraph("Địa chỉ: " + order.getReceiverAddress()).setFontSize(9));
+            document.add(new Paragraph("Mã giảm giá: " + (order.getPromotion() == null ? "Không có mã giảm giá" : order.getPromotion().getName())).setFontSize(9));
+            document.add(new Paragraph("Đơn vị vận chuyển: " + order.getShippingUnit().getName()).setFontSize(9));
+            document.add(new Paragraph("Trạng thái thanh toán: " + (order.getPaymentStatus() == 0 ? "Chưa thanh toán" : "Đã thanh toán")).setFontSize(9));
             document.add(new Paragraph("Trạng thái: " +
                     (order.getStatus() == 0 ? "Đang xử lý" :
                             (order.getStatus() == 1 ? "Đang vận chuyển" :
                                     (order.getStatus() == 2 ? "Đã hoàn thành" :
-                                            (order.getStatus() == 3 ? "Đã hủy" : "Không xác định"))))));
-            document.add(new Paragraph("Tổng giá trị: " + formatter.format(order.getPrice()) + " VND").setMarginBottom(20));
+                                            (order.getStatus() == 3 ? "Đã hủy" : "Không xác định")))))
+                    .setFontSize(9));
+            document.add(new Paragraph("Tổng giá trị: " + formatter.format(order.getPrice()) + " VND").setFontSize(9).setMarginBottom(10));
 
             // Tạo bảng sản phẩm
             Table table = new Table(UnitValue.createPercentArray(new float[]{4, 2, 2, 2}));
             table.setWidth(UnitValue.createPercentValue(100));
+            table.addHeaderCell("Tên sản phẩm").setFontSize(9);
+            table.addHeaderCell("Số lượng").setFontSize(9);
+            table.addHeaderCell("Giá (VND)").setFontSize(9);
+            table.addHeaderCell("Thành tiền (VND)").setFontSize(9);
 
-            // Thêm tiêu đề cột
-            table.addHeaderCell("Tên sản phẩm");
-            table.addHeaderCell("Số lượng");
-            table.addHeaderCell("Giá (VND)");
-            table.addHeaderCell("Thành tiền (VND)");
-
-            // Thêm dữ liệu sản phẩm vào bảng
             for (OrderDetail item : order.getOrderDetails()) {
-                table.addCell(item.getProduct().getName());
-                table.addCell(String.valueOf(item.getQuantity()));
-                table.addCell(String.valueOf(formatter.format(item.getPrice())));
-                table.addCell(String.valueOf(formatter.format(
-                        new BigDecimal(item.getQuantity()).multiply(item.getPrice()))));
+                table.addCell(new Paragraph(item.getProduct().getName()).setFontSize(9));
+                table.addCell(new Paragraph(String.valueOf(item.getQuantity())).setFontSize(9));
+                table.addCell(new Paragraph(formatter.format(item.getPrice())).setFontSize(9));
+                table.addCell(new Paragraph(formatter.format(new BigDecimal(item.getQuantity()).multiply(item.getPrice()))).setFontSize(9));
             }
 
-            // Thêm bảng vào document
-            document.add(table);
-            // Thêm chữ ký vào chân trang
-            document.add(new Paragraph("\n\n\n")
-                    .setTextAlignment(TextAlignment.RIGHT)); // Khoảng trắng trước chữ ký
+            document.add(table.setMarginBottom(10));
+
+            // Chữ ký và lời cảm ơn
             document.add(new Paragraph("Người lập hóa đơn")
                     .setBold()
+                    .setFontSize(9)
                     .setTextAlignment(TextAlignment.RIGHT));
             document.add(new Paragraph("(Ký, ghi rõ họ tên)")
                     .setItalic()
+                    .setFontSize(9)
                     .setTextAlignment(TextAlignment.RIGHT)
+                    .setMarginBottom(5));
+            document.add(new Paragraph("Trân trọng cảm ơn quý khách hàng!")
+                    .setBold()
+                    .setFontSize(10)
+                    .setTextAlignment(TextAlignment.CENTER)
                     .setMarginTop(10));
 
-
-            // Đóng document
             document.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,4 +125,14 @@ public class OrderPDFService {
 
         return out;
     }
+
+    private String generateRandomInvoiceNumber(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
+    }
 }
+
